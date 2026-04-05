1fc45de905d9b694c2146779ab009b9ad1ba4b08
Sato Katsumasa <katsumasa-sato@outlook.com>
2026-04-05 13:28:33 +0900

    mockのimageを修正

--- .storybook/mocks/nextImage.tsx
+++ .storybook/mocks/nextImage.tsx
@@ -1,16 +1,19 @@
-import NextImage from 'next/image';
-
 type Props = {
   src: string;
   alt: string;
   width?: number;
   height?: number;
+  fill?: boolean;
+  sizes?: string;
   className?: string;
   [key: string]: unknown;
 };
 
-const Image = ({ src, alt, width = 500, height = 500, className }: Props) => {
-  return <NextImage src={src} alt={alt} width={width} height={height} className={className} />;
+const Image = ({ src, alt, width, height, fill, className }: Props) => {
+  if (fill) {
+    return <img src={src} alt={alt} className={className} style={{ objectFit: 'cover', width: '100%', height: '100%' }} />;
+  }
+  return <img src={src} alt={alt} width={width} height={height} className={className} />;
 };
 
 export default Image;
