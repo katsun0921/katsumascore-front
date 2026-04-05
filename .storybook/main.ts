3c805dfc48c261fba939de285d0ff03c5815c708
Sato Katsumasa <katsumasa-sato@outlook.com>
2026-04-05 20:45:50 +0900

    staticDirsを修正

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,7 +9,7 @@ const config: StorybookConfig = {
   stories: [
     '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  staticDirs: [{ from: publicDir, to: '/' }],
+  staticDirs: [{ from: publicDir, to: '/katsumascore-front' }],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-docs',
